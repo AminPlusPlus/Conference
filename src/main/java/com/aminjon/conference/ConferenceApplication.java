e0bde914f0bbb7e91f0d7fe0ebb5ac858bb25d5d
Aminjoni Abdullozoda <amin030296@gmail.com>
2020-02-09 11:05:21 -0600

    Set up environment DB

--- src/main/java/com/aminjon/conference/ConferenceApplication.java
+++ src/main/java/com/aminjon/conference/ConferenceApplication.java
@@ -2,12 +2,19 @@ package com.aminjon.conference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConferenceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String welcomePage(){
+		return "Welcome to the  Rest Api !";
+	}
 }
--- src/main/java/com/aminjon/conference/models/Speaker.java
+++ src/main/java/com/aminjon/conference/models/Speaker.java
@@ -1,6 +1,8 @@
 package com.aminjon.conference.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
@@ -8,6 +10,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Speaker {
 
     @Id
@@ -23,6 +26,7 @@ public class Speaker {
     private byte[] speaker_photo;
 
     @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
     List<Session> sessions;
 
     public Speaker() {
