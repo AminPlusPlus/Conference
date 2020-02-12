45c52a28dac76ffe8297f06de5e8de93fd1bdac0
Aminjoni Abdullozoda <amin030296@gmail.com>
2020-02-11 18:40:56 -0600

    Swagger Updated

--- src/main/java/com/aminjon/conference/config/SwaggerConfig.java
+++ src/main/java/com/aminjon/conference/config/SwaggerConfig.java
@@ -2,9 +2,13 @@ package com.aminjon.conference.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,9 +20,19 @@ public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Conference API")
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.aminjon.conference.controller"))
                 .paths(PathSelectors.any())
+                .build().apiInfo(apiEndPointsInfo());
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Conference REST API")
+                .description("Conference Management REST API")
+                .contact(new Contact("Aminjoni Abdullozoda", "", "amin030296@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
                 .build();
     }
 }
--- src/main/java/com/aminjon/conference/controller/SessionController.java
+++ src/main/java/com/aminjon/conference/controller/SessionController.java
@@ -2,6 +2,7 @@ package com.aminjon.conference.controller;
 
 import com.aminjon.conference.models.Session;
 import com.aminjon.conference.repositories.SessionRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,22 +17,26 @@ public class SessionController {
     @Autowired
     private SessionRepository sessionRepository;
 
+    @ApiOperation(value = "Get List of Sessions",response = Session.class)
     @GetMapping
     public List<Session> list(){
         return sessionRepository.findAll();
     }
 
+    @ApiOperation(value = "Get an session by Id")
     @GetMapping
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id){
         return sessionRepository.getOne(id);
     }
 
+    @ApiOperation(value = "Add an session")
     @PostMapping
     public Session create(@RequestBody final Session session){
         return  sessionRepository.saveAndFlush(session);
     }
 
+    @ApiOperation(value = "Delete an session")
     @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
     public void delete(@PathVariable Long id,HttpServletResponse response) {
 
@@ -43,6 +48,7 @@ public class SessionController {
         sessionRepository.deleteById(id);
     }
 
+    @ApiOperation(value = "Update an session")
     @RequestMapping(value = "{id}",method = RequestMethod.PUT)
     public Session update(@PathVariable Long id, @RequestBody Session session, HttpServletResponse response){
         if (!sessionRepository.existsById(id)) {
