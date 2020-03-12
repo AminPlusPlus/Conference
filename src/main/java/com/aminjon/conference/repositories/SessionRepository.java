4a6138f281e3bfd49a8dfee86d7b317da8475aa2
Aminjoni Abdullozoda <amin030296@gmail.com>
2020-03-12 10:04:31 -0500

    updateVersion

--- src/main/java/com/aminjon/conference/repositories/SessionRepository.java
+++ src/main/java/com/aminjon/conference/repositories/SessionRepository.java
@@ -1,7 +1,12 @@
 package com.aminjon.conference.repositories;
 
 import com.aminjon.conference.models.Session;
+import com.aminjon.conference.models.Speaker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session,Long> {
+
 }
